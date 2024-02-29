e41d994e06e5382f787eab3357bc313047c5f852
Abassy <abasskuku319@gmail.com>
2024-02-29 12:13:13 +0300

    Created dart functions

---
+++ DART_FUNCTIONS.dart
@@ -0,0 +1,22 @@
+//DART FUNCTIONS
+void addNumbers (double num1, int num2)
+{
+  double sum = num1 + num2;
+  print("the sum of num1 and num2 is $sum");
+}
+
+void myName()
+{
+  print("My  name is Abass Kuku Animeri.");
+}
+
+//main void
+void main()
+{
+  //function calling
+  //addNumbers(100.90, 1000);
+
+  myName();
+}
+
+
---
+++ WEEK1.dart
@@ -0,0 +1,41 @@
+void main ()
+{
+  //print("This is my first dart class");
+
+  //DART LESSION TWO
+  //declearing variables
+  /*
+  int  num1 = 500; //without decimal point
+  double num2 = 3.78; //with decimal point
+  num num3 = 50;
+  num num4 = 50.4;
+
+  num sum = num1 + num2 + num3 + num4;
+
+  //for sum
+  print("num 1 is $num1");
+  print("num 2 is $num2");
+  print("num 3 is $num3");
+  print("num 4 is $num4");
+
+  print("sum is $sum");
+  */
+
+  //DART STRING DATA TYPES
+  /*
+  String schoolName = "Power learn Project";
+  String address = "Nairobi";
+  String module = "Dart Programming";
+
+  //printing values
+  print("My School is $schoolName, located in $address I am currently learning $module ");
+  */
+
+  //DART BOOLEAN DATA TYPES
+  /*
+  bool isStudent = true;
+  print("My occupation as student is $isStudent");
+  */
+  
+}
+
