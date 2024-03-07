25c90ae37e948d8fe13fe285bf0dc474b81cddd1
Abassy <abasskuku319@gmail.com>
2024-03-07 19:07:56 +0300

    dart control flow

---
+++ DART_CONTROL_FLOW.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+//DART CONTROL FLOW
+/*In Dart, Control flow statement can be categorized mainly in three following ways.
+Decision-making statements
+Looping statements
+Jump statements*/
+//IF STATEMENT
+void main()
+{
+  //var age = 30;
+  //var age;
+  /*print("Enter the age value: ");
+  var age = int.parse(stdin.readLineSync()!);
+  if  (age > 18)
+  {
+    print("Abass is eligible to vote");
+  }
+  else
+  {
+    print("Abass is not eligible to vote to vote");
+  }*/
+
+ print("Enter any number: ");
+ int number = int.parse(stdin.readLineSync()!);
+ if  (number % 2 == 0)
+{
+  print("The number entered is even");
+}
+else
+{
+  print("The number entered is odd");
+}
+}
+
+
